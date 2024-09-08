487f44644c19a77a13aa6f30ff53df6bafa926c1
Luislopez1941 <jorg3luislopezc1941@hotmail.com>
2024-09-07 20:14:48 -0500

    third commit

--- src/components/main/Experience.tsx
+++ src/components/main/Experience.tsx
@@ -1,22 +1,32 @@
 import React from 'react'
 import './styles/Experience.css'
-import Angular from '../../utils/svgs/Angular'
-import AmazonWebServices from '../../utils/svgs/AWS'
-import Cloudflare from '../../utils/svgs/Cloudflare'
-import TypeScript from '../../utils/svgs/Typescript'
-import CSS from '../../utils/svgs/CSS'
-import Redux from '../../utils/svgs/Redux'
-import ReactSvg from '../../utils/svgs/React'
-import NestJS from '../../utils/svgs/Nestjs'
-import Python from '../../utils/svgs/Python'
-import MySQL from '../../utils/svgs/Mysql'
-import FastAPI from '../../utils/svgs/FastAPI'
-import Nextjs from '../../utils/svgs/Nextjs'
-import TailwindCSS from '../../utils/svgs/Tailwind'
-import Vue from '../../utils/svgs/Vue'
-import MongoDB from '../../utils/svgs/MongoDB'
-import JavaScript from '../../utils/svgs/Javascript'
-import Pinia from '../../assets/pinia.svg'
+
+
+import github from '../../assets/briefcase/svgs/github-dark.svg'
+import git from '../../assets/briefcase/svgs/git.svg'
+import ReactSvg from '../../assets/briefcase/svgs/react.svg'
+import Angular from '../../assets/briefcase/svgs/angular.svg'
+import Nextjs from '../../assets/briefcase/svgs/nextjs_icon_dark.svg'
+import Vue from '../../assets/briefcase/svgs/vue.svg'
+import Pinia from '../../assets/briefcase/svgs/pinia.svg'
+import Zustand from '../../assets/briefcase/svgs/zustand.svg'
+import Redux from '../../assets/briefcase/svgs/redux.svg'
+import CSS from '../../assets/briefcase/svgs/css.svg'
+import Bootstrap from '../../assets/briefcase/svgs/bootstrap.svg'
+import TailwindCSS from '../../assets/briefcase/svgs/tailwindcss.svg'
+import TypeScript from '../../assets/briefcase/svgs/typescript.svg'
+import JavaScript from '../../assets/briefcase/svgs/javascript.svg'
+import NestJS from '../../assets/briefcase/svgs/nestjs.svg'
+import Python from '../../assets/briefcase/svgs/python.svg'
+import FastAPI from '../../assets/briefcase/svgs/fastapi.svg'
+import MySQL from '../../assets/briefcase/svgs/mysql.svg'
+import MongoDB from '../../assets/briefcase/svgs/mongodb.svg'
+import AmazonWebServices from '../../assets/briefcase/svgs/aws.svg'
+import Cloudflare from '../../assets/briefcase/svgs/cloudflare.svg'
+
+
+
+
 
 
 const Experience: React.FC = () => {
@@ -32,16 +42,33 @@ const Experience: React.FC = () => {
                 <p>Reactorizava y optimizaba el codigo del front asi  mismo actualizaba los frameworks a  las ultimas
                     actualizaciones
                 </p>
-            </div>         
+            </div>
+            <div className='technologies___card_container'>
+                <img src={github} width={35} alt="icono de react"/>
+                <img src={git} width={35} alt="icono de react"/>
+                <img src={CSS} width={30} alt="icono de css" />
+                <img src={Bootstrap} width={40} alt="icono de boostrap" />
+                <img src={JavaScript} width={30} alt="icono de javascript" />
+            </div>      
         </div>
         <div className='experience__card'>
             <p>Hiplot</p>
             <p>Front-End</p>
             <div>
                 <p>
-                    Me encargo de refactorizar toda la parte del front del sistema interno de la empresa y de crear los diseños para los nuevos software de la empresa. 
-                    Dirijo la parte del front de los nuevos desarrollos.
+                    Dirigo la parte del fornt-end de los desarollos internos de la empresa
                 </p>
+                <div className='technologies___card_container'>
+                    <img src={github} width={35} alt="icono de react"/>
+                    <img src={git} width={35} alt="icono de react"/>
+                    <img src={ReactSvg} width={35} alt="icono de react"/>
+                    <img src={Angular} width={30} alt="icono de angular"/> 
+                    <img src={Zustand} width={40} alt="" />
+                    <img src={CSS} width={30} alt="icono de css" />
+                    <img src={Bootstrap} width={40} alt="icono de boostrap" />
+                    <img src={TypeScript} width={30} alt="icono de typescript" />
+                    <img src={JavaScript} width={30} alt="icono de javascript" />
+                </div>
             </div>
         </div>
 
@@ -52,14 +79,14 @@ const Experience: React.FC = () => {
             </div>
             <p className='type'>Full Stack</p>
             <div className='technologies___card_container'>
-                <ReactSvg />
-                <Redux />
-                <CSS />
-                <TypeScript />
-                <NestJS />
-                <AmazonWebServices />
-                <MongoDB />
-                <Cloudflare />
+                <img src={ReactSvg} width={35} alt="icono de react"/>
+                <img src={Redux} width={30} alt="icono de redux"/>
+                <img src={CSS} width={30} alt="icono de css" />
+                <img src={TypeScript} width={30} alt="icono de typescript" />
+                <img src={NestJS} width={30} alt="icono de nestjs" />
+                <img src={AmazonWebServices} width={30} alt="icono de amazon web services" />
+                <img src={MongoDB} width={15} alt="icono de mongodb" />
+                <img src={Cloudflare} width={40} alt="icono de cloudflare" />
             </div>
             <div>
                 <p>
@@ -68,7 +95,7 @@ const Experience: React.FC = () => {
                 </p>
             </div>
             <div className='btns__card_container'>
-                <a href="https://cms-01.vercel.app/login" className="btn__card" target="_blank" rel="noopener noreferrer">Ver CRM</a>
+                <a href="https://cms-01.vercel.app" className="btn__card" target="_blank" rel="noopener noreferrer">Ver CRM</a>
                 <button className='btn__card'>Ver Ecommer</button>
                 <a href="https://back-ecommerce-bjt2.onrender.com" className="btn__card" target="_blank" rel="noopener noreferrer">Doc Back</a>
             </div>
@@ -80,45 +107,19 @@ const Experience: React.FC = () => {
             </div>
             <p className='type'>Full Stack</p>
             <div className='technologies___card_container'>
-                <Nextjs />
-                <Redux />
-                <TailwindCSS />
-                <Python />
-                <FastAPI />
-                <AmazonWebServices />
-                <MySQL />
-                <Cloudflare />
+                <img src={Nextjs} width={35} alt="icono de nextjs"/>
+                <img src={Redux} width={35} alt="icono de redux"/>
+                <img src={TailwindCSS} width={35} alt="icono de tailwind"/>
+                <img src={Python} width={65} alt="icono de python"/>
+                <img src={FastAPI} width={35} alt="icono de fastapi"/>
+                <img src={AmazonWebServices} width={30} alt="icono de amazon web services" />
+                <img src={MySQL} width={35} alt="icono de mysql"/>
+                <img src={Cloudflare} width={40} alt="icono de cloudflare" />
             </div>
             <div>
                 <p>
                 Me encargué de realizar todo el análisis de la logística para crear la base de datos y, a partir de ahí, continuar con la creación del back y del frontend.
                 Además, me encargué de crear el entorno de desarrollo y de producción y de la seguridad en la nube.
-                </p>
-            </div>
-            <div className='btns__card_container'>
-                <a href="https://cms-02.vercel.app" className="btn__card" target="_blank" rel="noopener noreferrer">Ver CRM</a>
-                <button className='btn__card'>Ver Ecommer</button>
-                <a href="https://back-ecommerce-bjt2.onrender.com" className="btn__card" target="_blank" rel="noopener noreferrer">Doc Back</a>
-            </div>
-        </div>
-        <div className='experience__card'>
-            <div className='content__title'>
-                <p>Frelance</p>
-                <p className='crm'>Ecomerce</p>
-            </div>
-            <p className='type'>Full Stack</p>
-            <div className='technologies___card_container'>
-                <Angular /> 
-                <CSS />
-                <TypeScript />
-                <AmazonWebServices />
-                
-                <MongoDB />
-                <Cloudflare />
-            </div>
-            <div>
-                <p>
-                    De igual forma, me encargué de realizar todo el análisis de la logística para comenzar con la base de datos, back, front, desarrollo y producción.
                 </p>
             </div>
             <div className='btns__card_container'>
@@ -130,17 +131,42 @@ const Experience: React.FC = () => {
         <div className='experience__card'>
             <div className='content__title'>
                 <p>Frelance</p>
+                <p className='crm'>Ecomerce</p>
+            </div>
+            <p className='type'>Full Stack</p>
+            <div className='technologies___card_container'>
+                <img src={Angular} width={30} alt="icono de angular" /> 
+                <img src={CSS} width={30} alt="icono de css" />
+                  <img src={TypeScript} width={30} alt="icono de typescript" />
+                <img src={AmazonWebServices} width={30} alt="icono de amazon web services" />
+                <img src={MongoDB} width={15} alt="icono de mongodb" />
+                <img src={Cloudflare} width={40} alt="icono de cloudflare" />
+            </div>
+            <div>
+                <p>
+                    De igual forma, me encargué de realizar todo el análisis de la logística para comenzar con la base de datos, back, front, desarrollo y producción.
+                </p>
+            </div>
+            <div className='btns__card_container'>
+                <a href="https://cms-02.vercel.app" className="btn__card" target="_blank" rel="noopener noreferrer">Ver CRM</a>
+                <button className='btn__card'>Ver Ecommer</button>
+                <a href="https://back-ecommerce-bjt2.onrender.com" className="btn__card" target="_blank" rel="noopener noreferrer">Doc Back</a>
+            </div>
+        </div>
+        <div className='experience__card'>
+            <div className='content__title'>
+                <p>Frelance</p>
                 <p className='crm'>Algoritmos</p>
             </div>
             <p className='type'>Full Stack</p>
             <div className='technologies___card_container'>
-                <Vue /> 
-                <img src={Pinia} alt="" />
-                <TailwindCSS />
-                <JavaScript />
-                <AmazonWebServices />
-                <MongoDB />
-                <Cloudflare />
+                <img src={Vue} width={30} alt="icono de vue" />
+                <img src={Pinia} width={30} alt="icono de Pinia" />
+                <img src={TailwindCSS} width={35} alt="icono de tailwind"/>
+                <img src={JavaScript} width={30} alt="icono de javascript" />
+                <img src={AmazonWebServices} width={30} alt="icono de amazon web services" />
+                <img src={MongoDB} width={15} alt="icono de mongodb" />
+                <img src={Cloudflare} width={40} alt="icono de cloudflare" />
             </div>
             <div>
                 <p>
